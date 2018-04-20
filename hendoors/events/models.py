af833930beafe23a7b5bd0a43931c759a5721df4
Stephen James <stephenorjames@gmail.com>
2018-04-20 19:44:01 -0400

    Create Event model

--- hendoors/events/models.py
+++ hendoors/events/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Event(models.Model):
+    name = models.CharField(max_length=50)
+    code = models.CharField(max_length=10)
+    location = models.CharField(max_length=50)
+    time = models.DateTimeField()
+    voting_open = models.BooleanField(default=False)
