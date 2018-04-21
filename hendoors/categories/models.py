608495b15aa87ad4e11debe1a071205bc310583c
Stephen James <stephenorjames@gmail.com>
2018-04-20 20:29:36 -0400

    Create categories app with Category model

---
+++ hendoors/categories/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+from hendoors.events.models import Event
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+    event = models.ForeignKey(Event, models.CASCADE)
+    description = models.TextField()
+    image = models.ImageField(blank=True, upload_to='categories')
+    voting_open = models.NullBooleanField(help_text="Uses the event's default if not explicitly specified.")
+
+    class Meta:
+        verbose_name_plural = 'categories'
+
+    def __str__(self):
+        return self.name
--- hendoors/settings.py
+++ hendoors/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'hendoors.categories',
     'hendoors.events',
 ]
 
@@ -120,3 +121,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
