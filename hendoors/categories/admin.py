27473d5573f387f11a16a5195170fe4ca894235e
Stephen James <stephenorjames@gmail.com>
2018-04-20 21:44:59 -0400

    Add event name to category's listing in admin

--- hendoors/categories/admin.py
+++ hendoors/categories/admin.py
@@ -3,4 +3,6 @@ from django.contrib import admin
 from . import models
 
 
-admin.site.register(models.Category)
+@admin.register(models.Category)
+class CategoryModelAdmin(admin.ModelAdmin):
+    list_display = ('name', 'event')
