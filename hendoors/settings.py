7ebf2e22d3050455a9314717884a991b3aa090c5
Angus Lam <angushtlam@gmail.com>
2018-04-29 20:13:58 -0400

    Gah Django templates and styles

--- hendoors/settings.py
+++ hendoors/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'hendoors',
     'hendoors.categories',
     'hendoors.entries',
     'hendoors.events',
@@ -122,6 +123,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
