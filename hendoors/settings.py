05838a0f555fffdf38a4dc6eecb09f3467339d90
Stephen James <stephenorjames@gmail.com>
2018-05-01 02:21:46 -0400

    Add static root to gitignore

--- hendoors/settings.py
+++ hendoors/settings.py
@@ -129,9 +129,8 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
-MEDIA_URL = '/media/'
-
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 
 LOGIN_URL = 'slack_auth'
