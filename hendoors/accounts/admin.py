fc5cffab741ead1a104b8013fb78b32cced82f7c
Stephen James <stephenorjames@gmail.com>
2018-05-03 23:25:28 -0400

    Improve admin view for User models

--- hendoors/accounts/admin.py
+++ hendoors/accounts/admin.py
@@ -1,8 +1,25 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
 from . import models
 
 
 @admin.register(models.User)
-class UserAdmin(admin.ModelAdmin):
-    exclude = ('password', 'extras')
+class UserAdmin(BaseUserAdmin):
+    fieldsets = (
+        ('Personal info', {'fields': ('email', 'name')}),
+        (None, {'fields': ('password',)}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser',
+                                    'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('date_joined', 'last_login')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'name', 'password1', 'password2'),
+        }),
+    )
+    list_display = ('name', 'email', 'is_staff', 'is_superuser')
+    ordering = ('name', 'email')
+    readonly_fields = ('date_joined', 'last_login')
+    search_fields = ('name', 'email')
