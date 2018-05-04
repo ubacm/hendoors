3d8fa95ed3e00c9158155c7cc13f14240603a2b1
Stephen James <stephenorjames@gmail.com>
2018-05-04 00:15:53 -0400

    Make the event code optional for now

--- hendoors/events/models.py
+++ hendoors/events/models.py
@@ -4,7 +4,9 @@ from django.urls import reverse
 
 class Event(models.Model):
     name = models.CharField(max_length=50)
-    code = models.CharField(max_length=10)
+    code = models.CharField(
+        max_length=10, blank=True,
+        help_text='The check-in code for the event; not currently used.')
     description = models.TextField()
     location = models.CharField(max_length=50)
     time = models.DateTimeField()
