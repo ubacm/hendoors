955f01f93826362c9249c34cfb532b9ca4f9a127
Stephen James <stephenorjames@gmail.com>
2018-05-05 04:11:36 -0400

    Increase Event.name max_length to 100

--- hendoors/events/models.py
+++ hendoors/events/models.py
@@ -3,7 +3,7 @@ from django.urls import reverse
 
 
 class Event(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=100)
     code = models.CharField(
         max_length=10, blank=True,
         help_text='The check-in code for the event; not currently used.')
