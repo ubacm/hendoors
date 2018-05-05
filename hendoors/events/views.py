74363aaabc54f219aa3bb670417f1003fdfac0e3
Stephen James <stephenorjames@gmail.com>
2018-05-05 14:21:26 -0400

    Add links to create entries

--- hendoors/events/views.py
+++ hendoors/events/views.py
@@ -6,6 +6,12 @@ from .models import Event
 class EventDetailView(DetailView):
     model = Event
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['has_categories_accepting_entries'] = (
+            self.object.category_set.filter(is_accepting_entries=True).exists())
+        return context
+
 
 class EventListView(ListView):
     model = Event
