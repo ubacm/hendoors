0688867a618e4854e2b1a34212f64ff2fa42573c
Stephen James <stephenorjames@gmail.com>
2018-05-04 22:44:04 -0400

    Add entry_voted_for to the context of CategoryDetailView

--- hendoors/categories/views.py
+++ hendoors/categories/views.py
@@ -1,10 +1,19 @@
 from django.views.generic import DetailView, ListView
 
+from hendoors.entries.models import Entry
 from .models import Category
 
 
 class CategoryDetailView(DetailView):
     model = Category
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['entry_voted_for'] = Entry.objects.filter(
+            vote__category=self.object,
+            vote__user=self.request.user,
+        ).first()
+        return context
 
 
 class CategoryListView(ListView):
